9a66a589060ac1858987a76036c02e94f4f9d58d
andregod <andre_godinho1@hotmail.com>
2023-07-20 10:53:33 +0100

    Criação da lista despesas, adaptação do adapter e inserção de cursores nas despesas

---
+++ app/src/main/java/com/example/appbudgetv2/AdapterDespesas.kt
@@ -0,0 +1,24 @@
+package com.example.appbudgetv2
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class AdapterDespesas : RecyclerView.Adapter<AdapterDespesas.ViewHolderDespesa>()  {
+
+    inner class ViewHolderDespesa(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolderDespesa {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolderDespesa, position: Int) {
+        TODO("Not yet implemented")
+    }
+}
--- app/src/main/java/com/example/appbudgetv2/ListaDespesasFragment.kt
+++ app/src/main/java/com/example/appbudgetv2/ListaDespesasFragment.kt
@@ -1,59 +1,74 @@
 package com.example.appbudgetv2
 
+import android.database.Cursor
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.loader.app.LoaderManager
+import androidx.loader.content.CursorLoader
+import androidx.loader.content.Loader
+import androidx.recyclerview.widget.LinearLayoutManager
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+private const val ID_LOADER_LIVROS = 0
 
 /**
  * A simple [Fragment] subclass.
  * Use the [ListaDespesasFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class ListaDespesasFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+class ListaDespesasFragment : Fragment(), LoaderManager.LoaderCallbacks<Cursor>  {
 
+    private var _binding: FragmentListaDespesasBinding? = null
+    private val binding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_lista_despesas, container, false)
+        _binding = FragmentListaDespesasBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val adapterDespesas = AdapterDespesas()
+        binding.recyclerViewDespesas.adapter = adapterDespesas
+        binding.recyclerViewDespesas.layoutManager = LinearLayoutManager(requireContext())
+
+        val loader = LoaderManager.getInstance(this)
+        loader.initLoader(ID_LOADER_LIVROS, null, this)
+    }
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ListaDespesasFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ListaDespesasFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+
+    }
+
+    override fun onCreateLoader(id: Int, args: Bundle?): Loader<Cursor> {
+        return CursorLoader(
+            requireContext(),
+            BudgetContentProvider.ENDERECO_DESPESA,
+            TabelaDespesas.CAMPOS,
+            null, null,
+            TabelaDespesas.CAMPO_NOME_DESPESA
+        )
+    }
+
+    override fun onLoaderReset(loader: Loader<Cursor>) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onLoadFinished(loader: Loader<Cursor>, data: Cursor?) {
+        TODO("Not yet implemented")
     }
 }
